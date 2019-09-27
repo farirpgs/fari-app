a35149ddfd628846d2dd56164256251ec0958114
Rene-Pier Deshaies-Gelinas <rene-pier.deshaies-gelinas@plusgrade.com>
2019-09-26 23:16:07 -0400

    fix: I said dice no dices

--- lib/routes/Dice/Dice.tsx
+++ lib/routes/Dice/Dice.tsx
@@ -1,17 +1,16 @@
 import Paper from "@material-ui/core/Paper";
 import React from "react";
-import { DnDDice } from "../../components/Dice/DnDDice";
-import { DungeonWorldDice } from "../../components/Dice/DungeonWorldDice";
 import { FudgeDice } from "../../components/Dice/FudgeDice";
 import { Page } from "../../components/Page/Page";
 
 export const Dice = () => {
   return (
-    <Page h1="Dices">
+    <Page h1="Dice">
       <Paper style={{ padding: "1rem", marginBottom: "2rem" }}>
         <h2 className="margin-1">Fudge</h2>
         <FudgeDice />
       </Paper>
+      {/* 
       <Paper style={{ padding: "1rem", marginBottom: "2rem" }}>
         <h2 className="margin-1">DnD</h2>
         <DnDDice />
@@ -19,7 +18,8 @@ export const Dice = () => {
       <Paper style={{ padding: "1rem", marginBottom: "2rem" }}>
         <h2 className="margin-1">Dungeon World</h2>
         <DungeonWorldDice />
-      </Paper>
+      </Paper> 
+      */}
     </Page>
   );
 };
