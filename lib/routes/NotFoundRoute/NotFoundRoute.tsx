1ec1e60416db1dc80f084db817e724a1fb58339f
Rene-Pier Deshaies-Gelinas <rene-pier.deshaies-gelinas@plusgrade.com>
2019-09-27 08:01:13 -0400

    feat: better 404

--- lib/routes/NotFoundRoute/NotFoundRoute.tsx
+++ lib/routes/NotFoundRoute/NotFoundRoute.tsx
@@ -1,8 +1,21 @@
+import { Divider, Paper } from "@material-ui/core";
 import React from "react";
+import { Banner } from "../../components/Banner/Banner";
+import { Page } from "../../components/Page/Page";
 export function NotFoundRoute(props) {
   return (
     <div>
-      <h1>Not Found</h1>
+      <Page isLoading={false}>
+        <Banner variant="warning">
+          <Paper style={{ padding: "2rem" }}>
+            <div className="h2">There is no war in Ba Sing Se</div>
+
+            <p>The page you are trying to access doesn't exist.</p>
+            <Divider className="margin-1"></Divider>
+            <p>Use the bottom menu to get out of the woods</p>
+          </Paper>
+        </Banner>
+      </Page>
     </div>
   );
 }
