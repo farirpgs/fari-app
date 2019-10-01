b859cc56e9dafd8b50affe96306efe2e979f9f72
Rene-Pier Deshaies-Gelinas <rene-pier.deshaies-gelinas@plusgrade.com>
2019-09-30 20:10:27 -0400

    feat: parcel 2

--- lib/routes/Home/Home.tsx
+++ lib/routes/Home/Home.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Page } from "../../components/Page/Page";
-import PageContent from "./homeContent.mdx";
 
 export const Home = props => {
   return (
     <Page h1={"Fari "}>
       <div className="markdown-body">
-        <PageContent></PageContent>
+        Welcome and thanks for using Fari! Fari is a web application that helps
+        you create and manage everything related to FATE Core and FATE
+        Accelerated. Create and update characters, add scenes for you scenarios
+        and roll some dice.
       </div>
     </Page>
   );
