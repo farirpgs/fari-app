be86186063d6998d8cc80be04d378b399eda0845
Rene-Pier Deshaies-Gelinas <rene-pier.deshaies-gelinas@plusgrade.com>
2019-12-15 12:50:08 -0500

    feat: kofi componetn

---
+++ lib/components/Kofi/Kofi.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const Kofi: React.FC<{}> = props => {
+  return (
+    <>
+      <a href="https://ko-fi.com/B0B4AHLJ" target="_blank">
+        <img
+          height="36"
+          style={{ border: "0px", height: "36px" }}
+          src="https://az743702.vo.msecnd.net/cdn/kofi1.png?v=2"
+          alt="Buy Me a Coffee at ko-fi.com"
+        />
+      </a>
+    </>
+  );
+};
