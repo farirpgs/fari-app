24e47400e288bc00668df6d80d23098cb024f5a2
Rene-Pier Deshaies-Gelinas <rene-pier.deshaies-gelinas@plusgrade.com>
2020-04-13 15:57:43 -0400

    dependency update

--- lib/routes/About/About.tsx
+++ lib/routes/About/About.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 import { Page } from "../../components/Page/Page";
-import AboutContent from "./About.mdx";
+// import AboutContent from "./About.mdx";
 
-export const About: React.FC<{}> = props => {
-  return (
-    <Page h1="About">
-      <AboutContent></AboutContent>
-    </Page>
-  );
+export const About: React.FC<{}> = (props) => {
+  return <Page h1="About">{/* <AboutContent></AboutContent> */}</Page>;
 };
