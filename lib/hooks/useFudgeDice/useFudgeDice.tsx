7e6a5ff5dd0a90a97ee7726169e0c480ac748ee4
Rene-Pier Deshaies-Gelinas <rene-pier.deshaies-gelinas@plusgrade.com>
2020-04-17 13:32:13 -0400

    fix: fudge dice color not refresh

--- lib/hooks/useFudgeDice/useFudgeDice.tsx
+++ lib/hooks/useFudgeDice/useFudgeDice.tsx
@@ -3,53 +3,52 @@ import { useEffect, useRef, useState } from "react";
 import { Dice } from "../../domains/dice/Dice";
 
 export function useFudgeDice(rolls: Array<number>) {
-  const theme = useTheme();
-  const [roll, setRoll] = useState(undefined);
-  const intervalId = useRef(undefined);
-  const refreshCount = useRef(0);
-  const finishedRolling = intervalId.current === undefined;
-  const color = getColor();
   const [realRoll] = rolls;
 
+  const theme = useTheme();
+  const [roll, setRoll] = useState<number>(undefined);
+  const [rolling, setRolling] = useState(false);
+  const [color, setColor] = useState("inherit");
+  const refreshCount = useRef(0);
+
   useEffect(() => {
+    let newColor = "inherit";
+    if (rolling) {
+      newColor = "inherit";
+    } else if (realRoll >= 3) {
+      newColor = theme.palette.success.main;
+    } else if (realRoll <= -3) {
+      newColor = theme.palette.error.main;
+    }
+    setColor(newColor);
+  }, [rolling, realRoll]);
+
+  useEffect(() => {
+    let intervalId: NodeJS.Timer = undefined;
     if (realRoll !== undefined) {
-      intervalId.current = setInterval(() => {
+      setRolling(true);
+      intervalId = setInterval(() => {
         if (refreshCount.current !== 50) {
           const fakeRoll = Dice.rollFudgeDice();
           refreshCount.current++;
           setRoll(fakeRoll);
         } else {
-          clearInterval(intervalId.current);
-          intervalId.current = undefined;
+          clearInterval(intervalId);
+          setRolling(false);
           refreshCount.current = 0;
           setRoll(realRoll);
         }
       }, 25);
     }
     return () => {
-      clearInterval(intervalId.current);
-      intervalId.current = undefined;
+      clearInterval(intervalId);
     };
   }, [rolls.length]);
-
-  function getColor() {
-    if (!finishedRolling) {
-      return "inherit";
-    }
-    if (roll >= 3) {
-      return theme.palette.success.main;
-    }
-    if (roll <= -3) {
-      return theme.palette.error.main;
-    }
-    return "inherit";
-  }
 
   return {
     state: {
       roll,
       color,
-      finishedRolling,
     },
   };
 }
