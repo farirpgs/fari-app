c7b48bea1f620ed59ba134e4d3e85ff91828ab0a
Rene-Pier Deshaies-Gelinas <rene-pier.deshaies-gelinas@plusgrade.com>
2020-05-08 16:38:27 -0400

    refac: remove useless code

--- lib/routes/Games/GamesPure.tsx
+++ lib/routes/Games/GamesPure.tsx
@@ -7,40 +7,37 @@ import ListItemText from "@material-ui/core/ListItemText";
 import GamesIcons from "@material-ui/icons/Games";
 import React from "react";
 import { AppLink } from "../../components/AppLink/AppLink";
-import { AppPaper } from "../../components/AppPaper/AppPaper";
 import { Page } from "../../components/Page/Page";
 import { IGame } from "../../types/IGame";
 
 export const GamesPure: React.FC<{ games: Array<IGame> }> = (props) => {
   return (
     <Page>
-      <AppPaper>
-        <p>
-          To create characters, select one of our supported game system below
-          first.
-        </p>
+      <p>
+        To create characters, select one of our supported game system below
+        first.
+      </p>
 
-        <List component="nav">
-          {props.games.map((game, index) => (
-            <AppLink to={`/game/${game.slug}`} key={game.slug}>
-              <ListItem
-                button
-                style={{
-                  zoom: "1.2",
-                }}
-              >
-                <ListItemAvatar>
-                  <Avatar>
-                    <GamesIcons />
-                  </Avatar>
-                </ListItemAvatar>
-                <ListItemText primary={game.name} />
-              </ListItem>
-              {index !== props.games.length - 1 && <Divider />}
-            </AppLink>
-          ))}
-        </List>
-      </AppPaper>
+      <List component="nav">
+        {props.games.map((game, index) => (
+          <AppLink to={`/game/${game.slug}`} key={game.slug}>
+            <ListItem
+              button
+              style={{
+                zoom: "1.2",
+              }}
+            >
+              <ListItemAvatar>
+                <Avatar>
+                  <GamesIcons />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary={game.name} />
+            </ListItem>
+            {index !== props.games.length - 1 && <Divider />}
+          </AppLink>
+        ))}
+      </List>
     </Page>
   );
 };
