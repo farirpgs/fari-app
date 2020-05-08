2af554995be3331681d647bfed19b68239a5e7c9
Rene-Pier Deshaies-Gelinas <rene-pier.deshaies-gelinas@plusgrade.com>
2020-05-08 16:23:04 -0400

    test: Font

--- lib/domains/font/Font.ts
+++ lib/domains/font/Font.ts
@@ -1,5 +1,8 @@
 export const Font = {
   lineHeight(fontSizeRem: number) {
+    if (!fontSizeRem) {
+      return "1rem";
+    }
     return `${fontSizeRem * 1.5}rem`;
   },
 };
---
+++ lib/domains/font/__tests__/Font.test.ts
@@ -0,0 +1,22 @@
+import { Font } from "../Font";
+
+describe("Font", () => {
+  describe("lineHeight", () => {
+    it("should always return a line height 1.5 bigger than the font size", () => {
+      // GIVEN
+      const fontSize = 1;
+      // WHEN
+      const result = Font.lineHeight(fontSize);
+      // THEN
+      expect(result).toEqual(`1.5rem`);
+    });
+    it("should fallback to 1rem if there's no fontSize", () => {
+      // GIVEN
+      const fontSize = undefined;
+      // WHEN
+      const result = Font.lineHeight(fontSize);
+      // THEN
+      expect(result).toEqual(`1rem`);
+    });
+  });
+});
