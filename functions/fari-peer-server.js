6065ece117668e06c4b5ce5877f9117f319e96bd
Rene-Pier Deshaies-Gelinas <rene-pier.deshaies-gelinas@plusgrade.com>
2020-05-25 21:26:03 -0400

    testing

--- functions/fari-peer-server.js
+++ functions/fari-peer-server.js
@@ -1,8 +1,15 @@
 const { PeerServer } = require("peer");
+const express = require("express");
+const serverless = require("serverless-http");
+const bodyParser = require("body-parser");
 
 const port = process.env.PORT || 9000;
 const path = "/";
+const peerServer = PeerServer({ port, path: path });
 
-console.log("Starting...");
-const peerServer = PeerServer({ port: 9000, path: path });
+const app = express();
+
+app.use(bodyParser);
+app.use("/peerjs", peerServer);
+module.exports.handler = serverless(app);
 console.log(`🚀 Started Peer Server http://localhost:${port}${path}`);
