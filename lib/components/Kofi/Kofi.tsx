42bbfff98c915fd72724af9b75dbc84542c0cf1a
RPDeshaies <rpdeshaies@gmail.com>
2020-05-27 14:42:41 -0400

    feat: 2.3.1 (ko-fi exception)

--- lib/components/Kofi/Kofi.tsx
+++ lib/components/Kofi/Kofi.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 
 export const Kofi: React.FC<{}> = (props) => {
-  const kofiwidget2 = window["kofiwidget2"];
-  kofiwidget2.init("Support Fari on Ko-fi", "#3f51b5", "B0B4AHLJ");
-  const html = kofiwidget2.getHTML();
+  let html: string = "";
+
+  try {
+    const kofiwidget2 = window["kofiwidget2"];
+    if (kofiwidget2) {
+      kofiwidget2.init("Support Fari on Ko-fi", "#3f51b5", "B0B4AHLJ");
+      html = kofiwidget2.getHTML();
+    }
+  } catch (error) {}
 
   return <div dangerouslySetInnerHTML={{ __html: html }}></div>;
 };
