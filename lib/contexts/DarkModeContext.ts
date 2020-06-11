f9f81c35ad7bed7eb2837ebd2c35d73bbbb717a9
Rene-Pier Deshaies Gelinas <rene-pierdeshaiesgelinas@Rene-Piers-Mac-mini.local>
2020-06-11 19:19:04 -0400

    refac: naming dark mode contect

--- lib/contexts/DarkModeContext.ts
+++ lib/contexts/DarkModeContext.ts
@@ -4,9 +4,13 @@ import React, { useEffect, useState } from "react";
 const key = "prefers-dark-mode";
 
 export function useDarkMode() {
-  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
-  const prefersDarkTheme = localStorage.getItem(key) === "true";
-  const [darkMode, setDarkMode] = useState(prefersDarkTheme || prefersDarkMode);
+  const prefersDarkModeFromBrowser = useMediaQuery(
+    "(prefers-color-scheme: dark)"
+  );
+  const prefersDarkThemeFromStorage = localStorage.getItem(key) === "true";
+  const defaultValue =
+    prefersDarkThemeFromStorage || prefersDarkModeFromBrowser;
+  const [darkMode, setDarkMode] = useState(defaultValue);
 
   useEffect(() => {
     localStorage.setItem(key, darkMode.toString());
