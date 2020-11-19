c2e9e7fde931574ff831f96aae370aac6a3a1c27
rpdeshaies <rpdeshaies@gmail.com>
2020-11-19 12:17:52 -0500

    fix: google analytics

--- lib/services/injections.ts
+++ lib/services/injections.ts
@@ -1,4 +1,3 @@
-import { GoogleAnalyticsService } from "./google-analytics/GoogleAnalyticsService";
 import { InternationalizationService } from "./internationalization/InternationalizationService";
 import { makeLogger } from "./logger/makeLogger";
 import { SentryService } from "./sentry/SentryService";
@@ -6,11 +5,9 @@ import { SentryService } from "./sentry/SentryService";
 const sentryService = new SentryService();
 const logger = makeLogger(sentryService);
 const internationalizationService = new InternationalizationService(logger);
-const googleAnalyticsService = new GoogleAnalyticsService(logger);
 
 export const injections = {
   internationalizationService,
   logger,
   sentryService,
-  googleAnalyticsService,
 };
