41ccf6e333f0404f9e962e3346fd77fc3b2483c5
rpdeshaies <rpdeshaies@gmail.com>
2020-11-19 12:16:48 -0500

    fix: disable blog

--- lib/blog/index.ts
+++ lib/blog/index.ts
@@ -1,10 +1,10 @@
 export const blogPosts = [
-  {
-    title: "New Drawing Area in Fari v3.2.0",
-    slug: "2020-10-19-fari-3-2-0-drawing-area",
-    date: "2020-10-16",
-    load: () => {
-      return import("./posts/2020-10-19-fari-3-2-0-drawing-area.md");
-    },
-  },
+  // {
+  //   title: "New Drawing Area in Fari v3.2.0",
+  //   slug: "2020-10-19-fari-3-2-0-drawing-area",
+  //   date: "2020-10-16",
+  //   load: () => {
+  //     return import("./posts/2020-10-19-fari-3-2-0-drawing-area.md");
+  //   },
+  // },
 ];
