ea466205c6b7d36cd24732f61bd380c7e5ee95b0
rpdeshaies <rpdeshaies@gmail.com>
2021-01-04 15:42:56 -0500

    fix: build

--- lib/routes/SrdsRoute/SrdsRoute.tsx
+++ lib/routes/SrdsRoute/SrdsRoute.tsx
@@ -1,4 +1,4 @@
-import { Container } from "@material-ui/core";
+import Container from "@material-ui/core/Container";
 import React from "react";
 import { Link } from "react-router-dom";
 import { Page } from "../../components/Page/Page";
