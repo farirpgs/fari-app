b5b77fb14ffb1c90cef44d5d794feb3c49f0e492
rpdeshaies <rpdeshaies@gmail.com>
2021-06-10 20:41:26 -0400

    fix: ci env

--- lib/constants/env.ts
+++ lib/constants/env.ts
@@ -1,7 +1,7 @@
 const context = import.meta.env?.MODE ?? "development";
 const isDev = import.meta.env?.DEV;
-const buildNumber = process.env.BUILD_NUMBER ?? "0";
-const hash = process.env.COMMIT_ID ?? "0";
+const buildNumber = import.meta.env?.VITE_GITHUB_RUN_NUMBER ?? "0";
+const hash = import.meta.env?.VITE_GITHUB_SHA ?? "0";
 const version = process.env.npm_package_version;
 const isTest = process.env.JEST_WORKER_ID;
 
