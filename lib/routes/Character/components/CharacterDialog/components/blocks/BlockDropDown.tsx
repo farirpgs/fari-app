604437dbbd6341b80148df40127efe8917aa411a
Jean-Loup Kahloun <jean-loup.kahloun@invenietis.com>
2021-10-15 14:50:31 -0400

    Moving all function in teh compoenent scope to have access to props directly

--- lib/routes/Character/components/CharacterDialog/components/blocks/BlockDropDown.tsx
+++ lib/routes/Character/components/CharacterDialog/components/blocks/BlockDropDown.tsx
@@ -39,31 +39,64 @@ export function BlockDropDown(
   const isLabelVisible =
     !!previewContentEditable({ value: props.block.label }) || props.advanced;
 
+  function handleDropDownItemChange(index: number, newValue: string) {
+    if (props.block.meta) {
+      const copy = [...props.block.meta.possibleValues];
+      const currentValue = copy[index];
+
+      copy[index] = newValue;
+
+      if (props.block.value === currentValue) {
+        props.onValueChange([newValue]);
+      }
+
+      props.onMetaChange({
+        ...props.block.meta,
+        possibleValues: [...copy],
+      });
+    }
+  }
+
+  function handleMoveDropDownItem(direction: Direction, index: number) {
+    if (props.block.meta) {
+      const copy = [...props.block.meta.possibleValues];
+
+      const targetIndex = direction === Direction.Down ? index + 1 : index - 1;
+
+      const targetValue = copy[targetIndex];
+      if (!targetValue) return;
+
+      copy[targetIndex] = copy[index];
+      copy[index] = targetValue;
+
+      props.onMetaChange({
+        ...props.block.meta,
+        possibleValues: [...copy],
+      });
+    }
+  }
+
   return (
     <>
       {!props.advanced && (
         <Box>
           <Grid container justify="space-between">
-            <Grid item>{isLabelVisible && renderLabel(props)}</Grid>
-            <Grid item>{renderDropDown(props)}</Grid>
+            <Grid item>{isLabelVisible && renderLabel()}</Grid>
+            <Grid item>{renderDropDown()}</Grid>
           </Grid>
         </Box>
       )}
 
       {props.advanced && (
         <Box>
-          {isLabelVisible && renderLabel(props)}
-          <div style={{ marginTop: "1em" }}>
-            {renderConfigurationPanel(t, props)}
-          </div>
+          {isLabelVisible && renderLabel()}
+          <div style={{ marginTop: "1em" }}>{renderConfigurationPanel(t)}</div>
         </Box>
       )}
     </>
   );
 
-  function renderLabel(
-    props: IBlockComponentProps<IDropDownBlock>
-  ): React.ReactElement<any, string | React.JSXElementConstructor<any>> {
+  function renderLabel() {
     return (
       <Box>
         <FateLabel display="inline">
@@ -81,9 +114,7 @@ export function BlockDropDown(
     );
   }
 
-  function renderDropDown(
-    props: IBlockComponentProps<IDropDownBlock>
-  ): React.ReactElement<any, string | React.JSXElementConstructor<any>> {
+  function renderDropDown() {
     return (
       <Box>
         {props.block.meta.asMultiple && (
@@ -140,10 +171,7 @@ export function BlockDropDown(
     );
   }
 
-  function renderConfigurationPanel(
-    t: any,
-    props: IBlockComponentProps<IDropDownBlock>
-  ): React.ReactElement<any, string | React.JSXElementConstructor<any>> {
+  function renderConfigurationPanel(t: any) {
     return (
       <Box>
         <Typography>
@@ -164,185 +192,128 @@ export function BlockDropDown(
                       data-cy={`${props.dataCy}.dropdown.item`}
                       value={value}
                       onChange={(newValue) => {
-                        handleDropDownItemChange(index, newValue, props);
+                        handleDropDownItemChange(index, newValue);
                       }}
                     />
                   </Grid>
-                  <Grid item>{renderGoUpButton(t, index, props)}</Grid>
-                  <Grid item>{renderGoDownButton(t, index, props)}</Grid>
-                  <Grid item>{renderRemoveButton(t, index, props)}</Grid>
+                  <Grid item>{renderGoUpButton(t, index)}</Grid>
+                  <Grid item>{renderGoDownButton(t, index)}</Grid>
+                  <Grid item>{renderRemoveButton(t, index)}</Grid>
                 </Grid>
               );
             })}
-          <div style={{ marginTop: "1em" }}>{renderAddButton(t, props)}</div>
+          <div style={{ marginTop: "1em" }}>{renderAddButton(t)}</div>
         </Typography>
       </Box>
     );
   }
-}
 
-function handleDropDownItemChange(
-  index: number,
-  newValue: string,
-  props: IBlockComponentProps<IDropDownBlock>
-) {
-  if (props.block.meta) {
-    const copy = [...props.block.meta.possibleValues];
-    const currentValue = copy[index];
-
-    copy[index] = newValue;
-
-    if (props.block.value === currentValue) {
-      props.onValueChange(newValue);
-    }
-
-    props.onMetaChange({
-      ...props.block.meta,
-      possibleValues: [...copy],
-    });
+  function renderGoUpButton(t: any, index: number) {
+    return (
+      <Tooltip
+        title={t("character-dialog.control.remove-dropdown-item")}
+        className={css({})}
+      >
+        <IconButton
+          size="small"
+          data-cy={`${props.dataCy}.remove-dropdown-item`}
+          onClick={() => {
+            handleMoveDropDownItem(Direction.Up, index);
+          }}
+        >
+          <ArrowUpwardRounded className={smallerIcon} />
+        </IconButton>
+      </Tooltip>
+    );
   }
-}
 
-function renderGoUpButton(
-  t: any,
-  index: number,
-  props: IBlockComponentProps<IDropDownBlock>
-) {
-  return (
-    <Tooltip
-      title={t("character-dialog.control.remove-dropdown-item")}
-      className={css({})}
-    >
-      <IconButton
-        size="small"
-        data-cy={`${props.dataCy}.remove-dropdown-item`}
-        onClick={() => {
-          handleMoveDropDownItem(Direction.Up, index, props);
-        }}
-      >
-        <ArrowUpwardRounded className={smallerIcon} />
-      </IconButton>
-    </Tooltip>
-  );
-}
+  function renderGoDownButton(t: any, index: number) {
+    return (
+      <Tooltip title={t("character-dialog.control.remove-dropdown-item")}>
+        <IconButton
+          size="small"
+          className={smallerButton}
+          data-cy={`${props.dataCy}.remove-dropdown-item`}
+          onClick={() => {
+            handleMoveDropDownItem(Direction.Down, index);
+          }}
+        >
+          <ArrowDownwardRounded className={smallerIcon} />
+        </IconButton>
+      </Tooltip>
+    );
+  }
 
-function renderGoDownButton(
-  t: any,
-  index: number,
-  props: IBlockComponentProps<IDropDownBlock>
-) {
-  return (
-    <Tooltip title={t("character-dialog.control.remove-dropdown-item")}>
-      <IconButton
-        size="small"
-        className={smallerButton}
-        data-cy={`${props.dataCy}.remove-dropdown-item`}
-        onClick={() => {
-          handleMoveDropDownItem(Direction.Down, index, props);
-        }}
-      >
-        <ArrowDownwardRounded className={smallerIcon} />
-      </IconButton>
-    </Tooltip>
-  );
+  function renderRemoveButton(t: any, index: number) {
+    return (
+      <Tooltip title={t("character-dialog.control.remove-dropdown-item")}>
+        <IconButton
+          size="small"
+          className={smallerButton}
+          data-cy={`${props.dataCy}.remove-dropdown-item`}
+          onClick={() => {
+            handleRemoveDropDownItem();
+          }}
+        >
+          <RemoveCircleOutlineIcon className={smallerIcon} />
+        </IconButton>
+      </Tooltip>
+    );
+
+    function handleRemoveDropDownItem() {
+      if (props.block.meta) {
+        const copy = [...props.block.meta.possibleValues];
+
+        if (props.block.value === copy[index]) {
+          props.onValueChange([props.block.meta.possibleValues[0]]);
+        }
+
+        copy.splice(index, 1);
+
+        props.onMetaChange({
+          ...props.block.meta,
+          possibleValues: [...copy],
+        });
+      }
+    }
+  }
+
+  function renderAddButton(t: any) {
+    return (
+      <Tooltip title={t("character-dialog.control.add-dropdown-item")}>
+        <IconButton
+          size="small"
+          className={smallerButton}
+          data-cy={`${props.dataCy}.remove-dropdown-item`}
+          onClick={() => {
+            handleAddDropDownItem(props);
+          }}
+        >
+          <AddCircleOutlineIcon />
+        </IconButton>
+      </Tooltip>
+    );
+
+    function handleAddDropDownItem(
+      props: IBlockComponentProps<IDropDownBlock>
+    ) {
+      if (props.block.meta) {
+        const copy = [...props.block.meta.possibleValues];
+
+        copy.push("new value");
+
+        props.onMetaChange({
+          ...props.block.meta,
+          possibleValues: [...copy],
+        });
+      }
+    }
+  }
 }
 
 enum Direction {
   Up,
   Down,
-}
-
-function handleMoveDropDownItem(
-  direction: Direction,
-  index: number,
-  props: IBlockComponentProps<IDropDownBlock>
-) {
-  if (props.block.meta) {
-    const copy = [...props.block.meta.possibleValues];
-
-    const targetIndex = direction === Direction.Down ? index + 1 : index - 1;
-
-    const targetValue = copy[targetIndex];
-    if (!targetValue) return;
-
-    copy[targetIndex] = copy[index];
-    copy[index] = targetValue;
-
-    props.onMetaChange({
-      ...props.block.meta,
-      possibleValues: [...copy],
-    });
-  }
-}
-
-function renderRemoveButton(
-  t: any,
-  index: number,
-  props: IBlockComponentProps<IDropDownBlock>
-) {
-  return (
-    <Tooltip title={t("character-dialog.control.remove-dropdown-item")}>
-      <IconButton
-        size="small"
-        className={smallerButton}
-        data-cy={`${props.dataCy}.remove-dropdown-item`}
-        onClick={() => {
-          handleRemoveDropDownItem(props);
-        }}
-      >
-        <RemoveCircleOutlineIcon className={smallerIcon} />
-      </IconButton>
-    </Tooltip>
-  );
-
-  function handleRemoveDropDownItem(
-    props: IBlockComponentProps<IDropDownBlock>
-  ) {
-    if (props.block.meta) {
-      const copy = [...props.block.meta.possibleValues];
-
-      if (props.block.value === copy[index]) {
-        props.onValueChange(props.block.meta.possibleValues[0]);
-      }
-
-      copy.splice(index, 1);
-
-      props.onMetaChange({
-        ...props.block.meta,
-        possibleValues: [...copy],
-      });
-    }
-  }
-}
-
-function renderAddButton(t: any, props: IBlockComponentProps<IDropDownBlock>) {
-  return (
-    <Tooltip title={t("character-dialog.control.add-dropdown-item")}>
-      <IconButton
-        size="small"
-        className={smallerButton}
-        data-cy={`${props.dataCy}.remove-dropdown-item`}
-        onClick={() => {
-          handleAddDropDownItem(props);
-        }}
-      >
-        <AddCircleOutlineIcon />
-      </IconButton>
-    </Tooltip>
-  );
-
-  function handleAddDropDownItem(props: IBlockComponentProps<IDropDownBlock>) {
-    if (props.block.meta) {
-      const copy = [...props.block.meta.possibleValues];
-
-      copy.push("new value");
-
-      props.onMetaChange({
-        ...props.block.meta,
-        possibleValues: [...copy],
-      });
-    }
-  }
 }
 
 BlockDropDown.displayName = "BlockDropDown";
