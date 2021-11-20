b7c3a553e1aa165332cabe3d397016a00ff5f3b1
rpdeshaies <rpdeshaies@gmail.com>
2021-11-20 09:03:38 -0500

    feat: sync

--- lib/services/liveblocks/makeLiveBlocksClient.ts
+++ lib/services/liveblocks/makeLiveBlocksClient.ts
@@ -6,13 +6,4 @@ export function makeLiveBlocksClient() {
       ? "pk_test_ETujS9FV5jeieP0ussc8xBkQ"
       : "pk_live_LQJHauN3xAZFOMO6XY7fscYK",
   });
-  // return createClient({
-  //   authEndpoint: async (room: string) => {
-  //     const test = import.meta.env.DEV ? "test=true" : "";
-  //     const result = await axios.get(
-  //       `/.netlify/functions/auth?room=${room}${test}`
-  //     );
-  //     return result.data.liveblocks.token;
-  //   },
-  // });
 }
