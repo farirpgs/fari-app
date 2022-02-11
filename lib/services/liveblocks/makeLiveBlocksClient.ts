5c1b4532e80b7d427986e96f2d057f37c24d49af
rpdeshaies <rpdeshaies@gmail.com>
2022-02-11 10:52:17 -0500

    fix: key

--- lib/services/liveblocks/makeLiveBlocksClient.ts
+++ lib/services/liveblocks/makeLiveBlocksClient.ts
@@ -4,7 +4,7 @@ import { env } from "../../constants/env";
 export function makeLiveBlocksClient() {
   return createClient({
     publicApiKey: env.isDev
-      ? "pk_live_LQJHauN3xAZFOMO6XY7fscYK"
+      ? "pk_test_ETujS9FV5jeieP0ussc8xBkQ"
       : "pk_live_LQJHauN3xAZFOMO6XY7fscYK",
   });
 }
