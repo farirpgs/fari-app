ddbff936c54a639194d83df45bc71b5b754eec44
rpdeshaies <rpdeshaies@gmail.com>
2022-03-07 16:43:18 -0500

    fix: disable autofreeze by ImmerJS

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,6 +1,9 @@
+import { setAutoFreeze } from "immer";
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
 import "./index.css";
+
+setAutoFreeze(false);
 
 ReactDOM.render(<App />, document.getElementById("root"));
