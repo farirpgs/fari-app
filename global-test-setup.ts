b6f088a0c80bbbc326da6d1d04b47f9765075b44
John Carmack <johncarmack@me.com>
2022-04-07 16:14:22 -0700

    Revert "as requested, placed fix in global-test-setup.ts"
    This reverts commit e8b0831c4f52d795e0545a5499318dcf611ada37.

--- global-test-setup.ts
+++ global-test-setup.ts
@@ -1,9 +1,3 @@
 module.exports = async () => {
   process.env.TZ = "UTC";
-
-  // fixes an implementation problem in Canvas
-  // that renders it difficult to test
-  HTMLCanvasElement.prototype.getContext = () => {
-    // from: https://stackoverflow.com/questions/48828759/unit-test-raises-error-because-of-getcontext-is-not-implemented
-  };
 };
