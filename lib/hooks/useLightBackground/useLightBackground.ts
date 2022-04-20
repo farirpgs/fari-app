e51bc87e0d2d257314472dd292002f8845773815
rpdeshaies <rpdeshaies@gmail.com>
2022-04-20 11:58:24 -0400

    chore: remove comments

--- lib/hooks/useLightBackground/useLightBackground.ts
+++ lib/hooks/useLightBackground/useLightBackground.ts
@@ -2,10 +2,6 @@ import { useTheme } from "@mui/material/styles";
 
 export function useLightBackground() {
   const theme = useTheme();
-  // const lightBackground =
-  //   theme.palette.mode === "light"
-  //     ? lighten(theme.palette.primary.light, 0.85)
-  //     : darken(theme.palette.primary.light, 0.75);
   const lightBackground =
     theme.palette.mode === "light" ? "#eff2f9" : "#272c34";
 
