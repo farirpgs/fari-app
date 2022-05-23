ba7a4e927a3d02f70a9f4449929e58870a802918
René-Pier Deshaies-Gélinas <renepier.dg@unity3d.com>
2022-05-23 18:12:00 -0400

    fix: router

--- lib/components/AppRouter/AppRouter.tsx
+++ lib/components/AppRouter/AppRouter.tsx
@@ -51,163 +51,153 @@ export const AppRouter = () => {
   return (
     <React.Suspense fallback={<LoadingRoute pathname={location.pathname} />}>
       <Routes>
-        <Route path={"/"} element={<HomeRoute />}>
-          <Route
-            path={"/characters/new/:category/:name"}
-            element={<NewCharacterRoute />}
-          />
-          <Route path={"/characters/:id"} element={<CharacterRoute />} />
-          <Route
-            path={"/characters/:id/print"}
-            element={<CharacterPrintRoute />}
-          />
-          <Route
-            path={"/dice"}
-            element={<DiceRoute pool={false} key="dice" />}
-          />
-          <Route
-            path={"/dice-pool"}
-            element={<DiceRoute pool={true} key="dice-pool" />}
-          />
-          <Route path={"/data"} element={<DataRoute />} />
-          <Route path={"/oracle"} element={<OracleRoute />} />
-          <Route path={"/draw"} element={<DrawRoute />} />
-          <Route
-            path={"play"}
-            element={
-              <Params
-                render={(params: { id: string }) => {
-                  const sessionId = params.id || userId;
-                  return (
-                    <LiveblocksProvider client={injections.liveBlocksClient}>
-                      <RoomProvider id={sessionId}>
-                        <PlayRoute />;
-                      </RoomProvider>
-                    </LiveblocksProvider>
-                  );
-                }}
-              />
-            }
-          />
-          ;
-          <Route
-            path={"play:id"}
-            element={
-              <Params
-                render={(params: { id: string }) => {
-                  const sessionId = params.id || userId;
-                  return (
-                    <LiveblocksProvider client={injections.liveBlocksClient}>
-                      <RoomProvider id={sessionId}>
-                        <PlayRoute />;
-                      </RoomProvider>
-                    </LiveblocksProvider>
-                  );
-                }}
-              />
-            }
-          />
-          ;
-          <Route
-            path={"/play/join/:id"}
-            element={
-              <Params
-                render={(params: { id: string }) => {
-                  const sessionId = params.id || userId;
-                  return (
-                    <LiveblocksProvider client={injections.liveBlocksClient}>
-                      <RoomProvider id={sessionId}>
-                        <JoinAGameRoute />;
-                      </RoomProvider>
-                    </LiveblocksProvider>
-                  );
-                }}
-              />
-            }
-          />
-          <Route path={"/play-offline"} element={<PlayOfflineRoute />} />
-          <Route path={"/scenes/:id"} element={<SceneRoute />} />
-          <Route path={"/cards/:id"} element={<CardCollection />} />
-          <Route
-            path="/srds/condensed"
-            element={
-              <ExternalRedirect url="https://fari.games/en/resources/fari-rpgs/fari-app-wiki" />
-            }
-          />
-          <Route
-            path="/srds/condensed"
-            element={
-              <ExternalRedirect url="https://fari.games/en/srds/evilhat/fate-condensed" />
-            }
-          />
-          <Route
-            path="/srds/accelerated"
-            element={
-              <ExternalRedirect url="https://fari.games/en/srds/evilhat/fate-accelerated" />
-            }
-          />
-          <Route
-            path="/srds/system-toolkit"
-            element={
-              <ExternalRedirect url="https://fari.games/en/srds/evilhat/fate-system-toolkit" />
-            }
-          />
-          <Route
-            path="/srds/adversary-toolkit"
-            element={
-              <ExternalRedirect url="https://fari.games/en/srds/evilhat/fate-adversary-toolkit" />
-            }
-          />
-          <Route
-            path="/fate-stunts"
-            element={
-              <ExternalRedirect url="https://fari.games/en/resources/fari-rpgs/fate-stunts" />
-            }
-          />
-          <Route
-            path="/fari-wiki"
-            element={
-              <ExternalRedirect url="https://fari.games/en/resources/fari-rpgs/fari-app-wiki" />
-            }
-          />
-          <Route
-            path="/seelie-squire"
-            element={
-              <ExternalRedirect url="https://fari.games/en/resources/seelie-squire/book-of-monsters" />
-            }
-          />
-          <Route
-            path="/success-with-style"
-            element={
-              <ExternalRedirect url="https://fari.games/en/resources/fari-rpgs/success-with-style" />
-            }
-          />
-          <Route
-            path="/blog/moments-in-fate"
-            element={
-              <ExternalRedirect url="https://fari.games/en/srds/fari-rpgs/success-with-style/moments-in-fate" />
-            }
-          />
-          <Route
-            path="/changelog"
-            element={
-              <ExternalRedirect url="https://fari.canny.io/changelog/" />
-            }
-          />
-          <Route
-            path={"/feature-requests"}
-            element={<FeatureRequestsRoute />}
-          />
-          <Route
-            path={"/feature-requests/*"}
-            element={<FeatureRequestsRoute />}
-          />
-          <Route path={"/bugs"} element={<BugsRoute />} />;
-          <Route path={"/bugs/*"} element={<BugsRoute />} />;
-          <Route path={"/story-builder"} element={<StoryBuilderRoute />} />
-          <Route path={"/story-dice"} element={<StoryDiceRoute />} />
-        </Route>
-
+        <Route path={"/"} element={<HomeRoute />} />
+        <Route
+          path={"/characters/new/:category/:name"}
+          element={<NewCharacterRoute />}
+        />
+        <Route path={"/characters/:id"} element={<CharacterRoute />} />
+        <Route
+          path={"/characters/:id/print"}
+          element={<CharacterPrintRoute />}
+        />
+        <Route path={"dice"} element={<DiceRoute pool={false} key="dice" />} />
+        <Route
+          path={"/dice-pool"}
+          element={<DiceRoute pool={true} key="dice-pool" />}
+        />
+        <Route path={"/data"} element={<DataRoute />} />
+        <Route path={"/oracle"} element={<OracleRoute />} />
+        <Route path={"/draw"} element={<DrawRoute />} />
+        <Route
+          path={"play"}
+          element={
+            <Params
+              render={(params: { id: string }) => {
+                const sessionId = params.id || userId;
+                return (
+                  <LiveblocksProvider client={injections.liveBlocksClient}>
+                    <RoomProvider id={sessionId}>
+                      <PlayRoute />;
+                    </RoomProvider>
+                  </LiveblocksProvider>
+                );
+              }}
+            />
+          }
+        />
+        ;
+        <Route
+          path={"play:id"}
+          element={
+            <Params
+              render={(params: { id: string }) => {
+                const sessionId = params.id || userId;
+                return (
+                  <LiveblocksProvider client={injections.liveBlocksClient}>
+                    <RoomProvider id={sessionId}>
+                      <PlayRoute />;
+                    </RoomProvider>
+                  </LiveblocksProvider>
+                );
+              }}
+            />
+          }
+        />
+        ;
+        <Route
+          path={"/play/join/:id"}
+          element={
+            <Params
+              render={(params: { id: string }) => {
+                const sessionId = params.id || userId;
+                return (
+                  <LiveblocksProvider client={injections.liveBlocksClient}>
+                    <RoomProvider id={sessionId}>
+                      <JoinAGameRoute />;
+                    </RoomProvider>
+                  </LiveblocksProvider>
+                );
+              }}
+            />
+          }
+        />
+        <Route path={"/play-offline"} element={<PlayOfflineRoute />} />
+        <Route path={"/scenes/:id"} element={<SceneRoute />} />
+        <Route path={"/cards/:id"} element={<CardCollection />} />
+        <Route
+          path="/srds/condensed"
+          element={
+            <ExternalRedirect url="https://fari.games/en/resources/fari-rpgs/fari-app-wiki" />
+          }
+        />
+        <Route
+          path="/srds/condensed"
+          element={
+            <ExternalRedirect url="https://fari.games/en/srds/evilhat/fate-condensed" />
+          }
+        />
+        <Route
+          path="/srds/accelerated"
+          element={
+            <ExternalRedirect url="https://fari.games/en/srds/evilhat/fate-accelerated" />
+          }
+        />
+        <Route
+          path="/srds/system-toolkit"
+          element={
+            <ExternalRedirect url="https://fari.games/en/srds/evilhat/fate-system-toolkit" />
+          }
+        />
+        <Route
+          path="/srds/adversary-toolkit"
+          element={
+            <ExternalRedirect url="https://fari.games/en/srds/evilhat/fate-adversary-toolkit" />
+          }
+        />
+        <Route
+          path="/fate-stunts"
+          element={
+            <ExternalRedirect url="https://fari.games/en/resources/fari-rpgs/fate-stunts" />
+          }
+        />
+        <Route
+          path="/fari-wiki"
+          element={
+            <ExternalRedirect url="https://fari.games/en/resources/fari-rpgs/fari-app-wiki" />
+          }
+        />
+        <Route
+          path="/seelie-squire"
+          element={
+            <ExternalRedirect url="https://fari.games/en/resources/seelie-squire/book-of-monsters" />
+          }
+        />
+        <Route
+          path="/success-with-style"
+          element={
+            <ExternalRedirect url="https://fari.games/en/resources/fari-rpgs/success-with-style" />
+          }
+        />
+        <Route
+          path="/blog/moments-in-fate"
+          element={
+            <ExternalRedirect url="https://fari.games/en/srds/fari-rpgs/success-with-style/moments-in-fate" />
+          }
+        />
+        <Route
+          path="/changelog"
+          element={<ExternalRedirect url="https://fari.canny.io/changelog/" />}
+        />
+        <Route path={"/feature-requests"} element={<FeatureRequestsRoute />} />
+        <Route
+          path={"/feature-requests/*"}
+          element={<FeatureRequestsRoute />}
+        />
+        <Route path={"/bugs"} element={<BugsRoute />} />;
+        <Route path={"/bugs/*"} element={<BugsRoute />} />;
+        <Route path={"/story-builder"} element={<StoryBuilderRoute />} />
+        <Route path={"/story-dice"} element={<StoryDiceRoute />} />
         <Route path="*" element={<NotFoundRoute />} />
       </Routes>
     </React.Suspense>
