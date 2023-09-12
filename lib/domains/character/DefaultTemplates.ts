d9d8ea05e4f93b0013a0658d2d331c4e077be0b8
rpdeshaies <rpdeshaies@gmail.com>
2023-09-12 12:47:43 -0400

    fix: defaults

--- lib/domains/character/DefaultTemplates.ts
+++ lib/domains/character/DefaultTemplates.ts
@@ -2,7 +2,4 @@ export const DefaultTemplates = {
   BlankTemplate: await import(
     "../../../public/character-templates/Defaults/Blank.json"
   ),
-  FateCondensed: await import(
-    "../../../public/character-templates/Fate Condensed/Fate Condensed.json"
-  ),
 } as const;
