0b371dbe39442a3cad1ae27c14b15ff4b7f407fe
Lynn Jones <lynn.jones.0702@gmail.com>
2026-01-12 16:13:01 -0600

    Update LiveblocksProvider API key handling

--- lib/components/AppRouter/AppRouter.tsx
+++ lib/components/AppRouter/AppRouter.tsx
@@ -82,8 +82,8 @@ export const AppRouter = () => {
                 return (
                   <LiveblocksProvider
                     publicApiKey={
-                      false ? "pk_dev_idze__Foj9T2QRR01oBR1su_4y-gl_mx8JwL90_FjQrsrtqxLnvJw5Q1GooxboHQ"
-                            : "pk_prod_DW7rLdIMrMduCwv9ETNWBueq2ih0mUFHJdeisqaw15jZtJrLb6Z_G80IjQS_kBW8",
+                       //"pk_dev_idze__Foj9T2QRR01oBR1su_4y-gl_mx8JwL90_FjQrsrtqxLnvJw5Q1GooxboHQ"
+                       "pk_prod_DW7rLdIMrMduCwv9ETNWBueq2ih0mUFHJdeisqaw15jZtJrLb6Z_G80IjQS_kBW8"
                     }
                   >
                     <RoomProvider
